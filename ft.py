195d35252c43d545a62dafc7c8fb5146907c5298
Manish <86160294+codereyinish@users.noreply.github.com>
2024-06-10 00:30:00 -0400

    "Feat: Create peft model and train it"
    Fine-tune text classifier with LoRA (1% trainable params), set hyperparameters (learning rate, batch size, epochs, weight decay), train PEFT model with tokenized datasets and custom metric (evaluation optional) and print the model's prediction labels

--- ft.py
+++ ft.py
@@ -94,13 +94,79 @@ for text in text_list:
       predictions = torch.argmax(logits)
     #convert number(label) from model into label name
       print(text + "- " + id2label[predictions.tolist()]) #converts tensors into list and then we map the numeric prediction to human text("positive", "negative", "neutral") with id2label dictionary
-# #LETS FINE-TUNE WITH LORA
-# #setup configuration for LoRA, it is done by creating object of LoraConfig class
-# peft_config = LoraConfig(...):
-# #Configuration parameters
-# task_type:"SEQ_CLS"
 
-        
+
+#LETS FINE-TUNE WITH LORA
+#setup configuration for LoRA, it is done by creating object of LoraConfig class
+peft_config= LoraConfig(task_type="SEQ_CLS",
+                        r=4,
+                        lora_alpha=32,
+                        lora_dropout=0.01,
+                        target_modules=['q_lin'])
+#Creating new version of model by changing few parameters with PEFT which we can call PEFT model, scale of trainable parameters was reduced to 100x , lets see what's up with the perfomance
+model = get_peft_model(model, peft_config)
+model.print_trainable_parameters()
+#trainable params: 628,994 || all params: 67,584,004 || trainable%: 0.9307, just 1% 
+
+#lets define hyperparmeters 
+lr = 1e-3 #0.001, size of optimization step
+batch_size = 4
+num_epochs = 10
+
+#defining Training Arguments
+training_args = TrainingArguments(
+      output_dir = model_checkpoint + "-lora-text-classification",
+      learning_rate = lr,
+      per_device_train_batch_size=batch_size,
+      per_device_eval_batch_size=batch_size,
+      num_train_epochs=num_epochs,
+      weight_decay = 0.01, #to prevent overfitting
+      evaluation_strategy="epoch",
+      save_strategy="epoch",
+      load_best_model_at_end = True,
+)
+
+# QUESTION . We have earlier defined learning rate ( lora_alpha) and droopout(to prevent overfiiting) for LoRA in peft configuration then why we are again defining it inside TrainingArguments, is it redundacy or are they both different so that we have to include twice, if different , does that mean whatever happening in LoRA is separate?
+
+#Now lets train. 🥳
+
+#create trainer object
+trainer = Trainer(
+      model = model, #our peft model
+      args = training_args, #hyperparameters ,defines how to do training
+      train_dataset=tokenize_dataset["train"], #tokenized training data
+      eval_dataset=tokenize_dataset["validation"], #tokenized validation data
+      tokenizer=tokenizer,
+      data_collator=data_collator, #adds padding on each batch 
+      compute_metrics=compute_metrics,#measure acccuracy on validation dataset
+)
+
+#train model
+trainer.train()
+
+#Trained Model Performance
+#move model to .mps for better GPU computation on Apple's Chip
+model.to('mps')
+print("Untrained_Model_Perfomance")
+print("---------------")
+for text in text_list:
+      inputs = tokenizer.encode(text, return_tensors="pt").to("mps")
+      logits = model(inputs).logits
+      predictions = torch.max(logits,1).indices
+      print(text + "-" + id2label[predictions.tolist()[0]]) #we get the array of predictions for each text in a batch_size , convert it into Python list, we only retrieve the current text in a text list i.e. [0] and convert it into human readable label.
+
+
+
+
+
+
+
+
+
+
+
+
+
     
       
 
